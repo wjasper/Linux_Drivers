fcd9a89aef7e12d64e22b1dabb8f7b09d502b2b5
Warren Jasper <wjasper@ncsu.edu>
2020-03-13 15:29:56 -0400

    Delete .das4020-12.ko.cmd

--- PCI/pci-das4020-12/.das4020-12.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/wjj/Linux_Drivers/PCI/pci-das4020-12/das4020-12.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/wjj/Linux_Drivers/PCI/pci-das4020-12/das4020-12.ko /home/wjj/Linux_Drivers/PCI/pci-das4020-12/das4020-12.o /home/wjj/Linux_Drivers/PCI/pci-das4020-12/das4020-12.mod.o ;  true
