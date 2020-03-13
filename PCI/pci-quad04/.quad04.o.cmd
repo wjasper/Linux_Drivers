be3a2bcc3cdd8b59f566291f9e47124f2f5a8a4a
Warren Jasper <wjasper@ncsu.edu>
2020-03-13 15:33:43 -0400

    Delete .quad04.o.cmd

--- PCI/pci-quad04/.quad04.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/wjj/Linux_Drivers/PCI/pci-quad04/quad04.o := ld -m elf_x86_64  -z max-page-size=0x200000    -r -o /home/wjj/Linux_Drivers/PCI/pci-quad04/quad04.o /home/wjj/Linux_Drivers/PCI/pci-quad04/LS7266R1.o
